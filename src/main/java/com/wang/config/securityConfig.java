0d7944b98ccfa526b5c5b4371d5863be33faf968
emmmwangxiaowang <954544828@qq.com>
2021-10-06 21:33:08 +0800

    权限控制

--- src/main/java/com/wang/config/securityConfig.java
+++ src/main/java/com/wang/config/securityConfig.java
@@ -31,6 +31,13 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
         //没有权限默认会到登录页面
         //需要开启登录的页面
         http.formLogin();
+
+        //防止网站攻击
+        //关闭csrf springboot默认开启  跨站点请求伪造
+        http.csrf().disable();
+
+        //开启注销功能
+        http.logout().deleteCookies("remove").invalidateHttpSession(false).logoutSuccessUrl("/");
     }
 
     //认证
@@ -38,7 +45,7 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     //在spring security5.0+ 中新增了很多加密方法
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //这些数据正常应该从数据库中读取
+        //这些数据正常应该从数据库中读取  这里使用的是内存数据inMemoryAuthentication
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                 .withUser("wang").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2", "vip3")
                 .and().withUser("yingying").password(new BCryptPasswordEncoder().encode("123456")).roles("vip2", "vip1");
